8e97682d2c6d58d9effd21134ffef6a501a77fcf
Marwana <bimp.marwana@gmail.com>
2017-06-02 19:45:03 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express     = require("express"),
+    bodyParser  = require("body-parser"),
+    // mongoose    = require("mongoose"),
+    method      = require("method-override"),
+    app         = express();
+
+    // Middleware
+    app.use(method("_method"));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(express.static("public"));
+    app.set("view engine", "ejs");
+
+    // // MongoDB Connection
+    // mongoose.connect('mongodb://bekraf:pwd123456!!@ds157641.mlab.com:57641/bekraf_nodedb');
+    // var campSchema = new mongoose.Schema({
+    //     title: String,
+    //     image: String,
+    //     description: String
+    // })
+
+app.get("/", function(request, response) {
+    response.render("index", {title: "CinemaScope"})
+});
+
+app.get("/newrelease", function(request, response) {
+    response.render("movielist", {title: "New Release", header: "New Release"})
+});
+
+app.get("/topboxoffice", function(request, response) {
+    response.render("movielist", {title: "Top Box Office", header: "Top 20 Box Office"})
+});
+
+app.get("/topfavourite", function(request, response) {
+    response.render("movielist", {title: "Top Favourite", header: "Top 20 Favourite"})
+});
+
+app.get("/movie/:id", function(request, response) {
+    response.render("movielist", {title: "Movie", header: "Top 20 Favourite"})
+});
+
+app.listen(3000, function(error) {
+    console.log("server starting")
+});
